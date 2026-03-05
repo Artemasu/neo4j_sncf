25ee114b5676b67795ca590f241391d45b3dfbc8
Artemasu <karl.juttin@gmail.com>
2026-03-05 14:32:06 +0100

    Base de donnée a tester

--- nettoyage.py
+++ nettoyage.py
@@ -1,26 +1,17 @@
 import pandas as pd
 
-def preparer_donnees(chemin_csv):
-    """
-    Sépare le CSV en deux structures (nœuds et relations).
-    On utilise .head(1000) pour ne garder que les 1000 premières lignes pour les tests.
-    """
-    # Lecture du fichier complet
+def preparer_donnees(chemin_csv): # Sépare le CSV en deux structures (noeuds et relations).
+
     df_complet = pd.read_csv(chemin_csv, sep=';')
+    df = df_complet.head(10000)
 
-    # --- ÉCHANTILLONNAGE POUR LES TESTS ---
-    # On ne garde que les 1000 premières lignes pour gagner du temps
-    # Supprimez ".head(1000)" pour importer la totalité des 346 925 trajets à la fin
-    df = df_complet.head(10000) 
-
-    # --- LISTE DES GARES (Nœuds) ---
     dep = df[['Origine', 'Origine IATA']].rename(columns={'Origine': 'ville', 'Origine IATA': 'iata'})
     arr = df[['Destination', 'Destination IATA']].rename(columns={'Destination': 'ville', 'Destination IATA': 'iata'})
     
-    # Dédoublonnage pour respecter l'unicité du schéma
+    # Dédoublonnage
     df_nodes = pd.concat([dep, arr]).drop_duplicates(subset=['iata']).dropna()
 
-    # --- LISTE DES TRAJETS (Relations) ---
+    # Liste des trajets (Relations)
     df_trajets = df[['Origine IATA', 'Destination IATA', 'TRAIN_NO', 'Heure_depart', 'Heure_arrivee', 'DATE']]
     
     return df_nodes, df_trajets
